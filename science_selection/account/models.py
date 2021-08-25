bc8599baadb716eb928f766e3dc6a9ee7cd58330
1NR_Operator_16 <markelovpa12@gmail.com>
2021-08-25 09:46:46 +0300

    added models for account app

--- science_selection/account/models.py
+++ science_selection/account/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    role_name = models.CharField(max_length=32)
+
+
+class Affiliation(models.Model):
+    pass
+
+
+class Member(models.Model):
+    class Validator:
+        phone_regex = RegexValidator(regex=r'^\+?\d{6,11}$',
+                                     message="Введите корректный номер телефона формата: +99999999999.")
+
+    role = models.ForeignKey(Role, on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    father_name = models.CharField(max_length=32)
+    phone = models.CharField(validators=[Validator.phone_regex], max_length=17, blank=True)
+    affiliation = models.ManyToManyField(Affiliation)
+
+
+class Booking(models.Model):
+    pass
+
+
+class Booking_Type(models.Model):
+    pass
