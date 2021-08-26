aa0fa367e4d1564df91dd9caf26980449a5b8fc8
1NR_Operator_16 <markelovpa12@gmail.com>
2021-08-26 12:37:24 +0300

    added translations

--- science_selection/account/models.py
+++ science_selection/account/models.py
@@ -7,11 +7,19 @@ from django.dispatch import receiver
 class Role(models.Model):
     role_name = models.CharField(max_length=32)
 
+    class Meta:
+        verbose_name = "Роль"
+        verbose_name_plural = "Роли"
+
 
 class Affiliation(models.Model):
     direction = models.ForeignKey('application.Direction', verbose_name="Направление", on_delete=models.CASCADE)
     company = models.IntegerField(verbose_name="Номер роты")
     platoon = models.IntegerField(verbose_name="Номер взвода")
+
+    class Meta:
+        verbose_name = "Принадлежность"
+        verbose_name_plural = "Принадлежности"
 
 
 class Member(models.Model):
@@ -24,6 +32,10 @@ class Member(models.Model):
     father_name = models.CharField(max_length=32, verbose_name="Отчество", blank=True)
     phone = models.CharField(validators=[Validator.phone_regex], verbose_name="Телефон", max_length=17)
     affiliations = models.ManyToManyField(Affiliation, verbose_name="Принадлежность", blank=True)
+
+    class Meta:
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
@@ -43,6 +55,10 @@ class BookingType(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+    class Meta:
+        verbose_name = "Тип бронирования"
+        verbose_name_plural = "Типы бронирования"
+
 
 class Booking(models.Model):
     booking_type = models.ForeignKey(BookingType, verbose_name="Тип бронирования", on_delete=models.DO_NOTHING)
@@ -50,7 +66,15 @@ class Booking(models.Model):
     slave = models.ForeignKey(Member, verbose_name="Кандидат", on_delete=models.CASCADE, related_name='candidate')
     affiliation = models.ForeignKey(Affiliation, verbose_name="Принадлежность", on_delete=models.DO_NOTHING)
 
+    class Meta:
+        verbose_name = "Бронирование"
+        verbose_name_plural = "Бронирования"
+
 
 class ActivationLink(models.Model):
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
     token = models.CharField(verbose_name="Токен", max_length=64)
+
+    class Meta:
+        verbose_name = "Ссылка активации"
+        verbose_name_plural = "Ссылки активации"
