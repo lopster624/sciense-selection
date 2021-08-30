cea4579a5c41798d646c0a355a4e27cd0d7400b4
1NR_Operator_16 <markelovpa12@gmail.com>
2021-08-30 12:32:10 +0300

    added templates and view

--- science_selection/application/views.py
+++ science_selection/application/views.py
@@ -1,13 +1,24 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import View
 from django.views.generic.list import ListView
 
-from .models import Direction
+from .models import Direction, Application
 
 
-class DirectionView(ListView):
+class DirectionView(LoginRequiredMixin, ListView):
     model = Direction
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['test'] = 'test'
         return context
+
+
+class ApplicationListView(LoginRequiredMixin, ListView):
+    model = Application
+
+
+class CompetenceEditView(LoginRequiredMixin, View):
+    def get(self, request):
+        pass
