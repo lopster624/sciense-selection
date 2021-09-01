5ec8c15652cd2ece488081dbe5005c9b2fd18c23
1NR_Operator_16 <markelovpa12@gmail.com>
2021-09-01 16:55:24 +0300

    merged all

--- science_selection/application/views.py
+++ science_selection/application/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.views.generic.list import ListView
 
