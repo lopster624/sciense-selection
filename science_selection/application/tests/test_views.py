474cb2df1cbd5cbd1037f0e9fa5355cff51d3726
1NR_Operator_16 <markelovpa12@gmail.com>
2021-10-15 11:37:43 +0300

    created all the tests for applicition_list view

--- science_selection/application/tests/test_views.py
+++ science_selection/application/tests/test_views.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-from django.contrib.auth.models import User
-from django.test import TestCase
-from django.urls import reverse
-
-from account.models import Affiliation, Member, Role, Booking, BookingType
+from account.models import Affiliation, Booking, BookingType
 from application.models import Direction, Education, Application
 from utils.calculations import get_current_draft_year
-from utils.constants import MASTER_ROLE_NAME, SLAVE_ROLE_NAME, BOOKED, IN_WISHLIST
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from account.models import Role, Member
+from utils.constants import SLAVE_ROLE_NAME, MASTER_ROLE_NAME, BOOKED, IN_WISHLIST
 
 
 class ApplicationListTest(TestCase):
@@ -25,19 +25,29 @@ class ApplicationListTest(TestCase):
         aff2 = Affiliation.objects.get(company=2, platoon=2)
         current_year, current_season = get_current_draft_year()
         for i in range(6):
-            cur_user = User.objects.create_user(username=f'testuser{i}', password='12345')
+            cur_user = User.objects.create_user(username=f'testuser{i}', last_name=f'testuser{i}', password='12345')
             cur_member = Member.objects.create(user=cur_user, role=slave_role, phone='89998887766')
             app = Application.objects.create(member=cur_member, birth_day=datetime.strptime('18/09/19', '%d/%m/%y'),
-                                             birth_place='Test', nationality='РФ', military_commissariat='Йо',
+                                             birth_place=f'Test{abs(i - 6)}', nationality='РФ',
+                                             military_commissariat='Йо',
                                              group_of_health='А1', draft_year=current_year, draft_season=i % 2 + 1)
-            Education.objects.create(application=app, education_type='b', university='МЭИ',
-                                     specialization='ИВТ',
-                                     avg_score=5, end_year=2020, is_ended=True,
-                                     name_of_education_doc='Высшее',
-                                     theme_of_diploma='Тест')
+            if i == 4:
+                Education.objects.create(application=app, education_type='b', university='МЭИ',
+                                         specialization='ИВТ',
+                                         avg_score=5, end_year=2020, is_ended=True,
+                                         name_of_education_doc='Высшее',
+                                         theme_of_diploma='Тест')
+            else:
+                Education.objects.create(application=app, education_type='b', university='МЭИ',
+                                         specialization='ИВТ',
+                                         avg_score=4, end_year=2020, is_ended=True,
+                                         name_of_education_doc='Высшее',
+                                         theme_of_diploma='Тест')
+            app.update_scores()
         application = Application.objects.get(member__user=User.objects.get(username='testuser1'))
         user2 = User.objects.get(username='testuser2')
         application.directions.add(aff1.direction)
+        application.update_scores()
         booked = BookingType.objects.create(name=BOOKED)
         in_wishlist = BookingType.objects.create(name=IN_WISHLIST)
         Booking.objects.create(booking_type=booked, master=master_member, slave=application.member,
@@ -83,12 +93,72 @@ class ApplicationListTest(TestCase):
         current_year, current_season = get_current_draft_year()
         self.assertEqual(list(resp.context['object_list']),
                          list(Application.objects.filter(draft_year=current_year, draft_season=current_season[0])))
-from django.test import TestCase
-from django.urls import reverse
-from django.contrib.auth.models import User
 
-from account.models import Role, Member
-from utils.constants import SLAVE_ROLE_NAME, MASTER_ROLE_NAME
+    def test_name_sort(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(reverse('application_list') + '?ordering=member__user__last_name')
+        self.assertEqual(list(resp.context['object_list']),
+                         list(Application.objects.all().order_by('member__user__last_name')))
+
+    def test_city_sort(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(reverse('application_list') + '?ordering=birth_place')
+        self.assertEqual(list(resp.context['object_list']),
+                         list(Application.objects.all().order_by('birth_place')))
+
+    def test_fullness_sort(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(reverse('application_list') + '?ordering=-fullness')
+        self.assertEqual(list(resp.context['object_list']),
+                         list(Application.objects.all().order_by('-fullness')))
+
+    def test_final_score_sort(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(reverse('application_list') + '?ordering=-final_score')
+        self.assertEqual(list(resp.context['object_list']),
+                         list(Application.objects.all().order_by('-final_score')))
+
+    def test_directions_filter(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(
+            reverse('application_list') + '?ordering=member__user__last_name&directions=2&directions=3')
+        app = Application.objects.get(member__user=User.objects.get(username='testuser1'))
+        self.assertEqual(resp.context['object_list'][0], app)
+
+    def test_booked_filter(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(
+            reverse('application_list') + '?ordering=member__user__last_name&affiliation=2')
+        app = Application.objects.get(member__user=User.objects.get(username='testuser1'))
+        self.assertEqual(resp.context['object_list'][0], app)
+        self.assertEqual(len(resp.context['object_list']), 1)
+
+    def test_wishlist_filter(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(
+            reverse('application_list') + '?ordering=member__user__last_name&in_wishlist=3')
+        app = Application.objects.get(member__user=User.objects.get(username='testuser2'))
+        self.assertEqual(resp.context['object_list'][0], app)
+        self.assertEqual(len(resp.context['object_list']), 1)
+
+    def test_draft_season_filter(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(
+            reverse('application_list') + '?ordering=member__user__last_name&draft_season=1')
+        self.assertEqual(len(resp.context['object_list']), 3)
+
+    def test_booked_wishlist_flags(self):
+        self.client.login(username='master', password='master')
+        resp = self.client.get(
+            reverse('application_list') + '?ordering=member__user__last_name&draft_season=1&draft_season=2')
+        booked = 0
+        wishlist = 0
+        for i in resp.context['object_list']:
+            if i.is_booked:
+                booked += 1
+            if i.is_in_wishlist:
+                wishlist += 1
+        self.assertEqual((wishlist, booked), (1, 1))
 
 
 class CreateApplicationViewTest(TestCase):
