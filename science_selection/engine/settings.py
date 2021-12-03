c98fb67ab8f58c6326a0faaaecadb075a72c68e7
Pavel <markelovpa12@gmail.com>
2021-12-03 16:40:06 +0300

    upd env

--- science_selection/engine/settings.py
+++ science_selection/engine/settings.py
@@ -13,7 +13,7 @@ import dotenv
 import os
 from pathlib import Path
 
-dotenv.load_dotenv('../.env')
+dotenv.load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DJANGO_DEBUG') in ['True', 'true', '1', True]:
